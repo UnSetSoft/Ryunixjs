0002a17e3e7492d257d25c08dc605f93a8e06c4b
KagariSoft-Dev <dev@kagarisoft.com>
2023-06-10 18:27:51 -0300

    create 7 files

---
+++ templates/ryunix-vanilla/src/pages/App.jsx
@@ -0,0 +1,16 @@
+import Ryunix, { Component } from "@unsetsoft/ryunixjs";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h2>This is my ReactJS Clone</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
