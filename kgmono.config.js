6e45a17793cad5fa1bc5d90807bc2bebc9fbdc4a
KagariSoft-Dev <dev@kagarisoft.com>
2023-06-10 13:40:46 -0300

    chore(Root): updated root

---
+++ kgmono.config.js
@@ -0,0 +1,9 @@
+/*
+Include a list of package.json, for example:
+* Important, don't add "../../../"
+[
+      "packages/test/package.json",
+      "packages/test2/package.json"
+]
+*/
+module.exports = ["packages/ryunix/package.json"];
