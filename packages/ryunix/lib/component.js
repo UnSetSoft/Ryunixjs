a7326e2d12b6705166ebb43efa40db9a2695df54
KagariSoft-Dev <dev@kagarisoft.com>
2023-06-13 03:54:55 -0300

    fix(fiber)

--- packages/ryunix/lib/component.js
+++ packages/ryunix/lib/component.js
@@ -2,15 +2,5 @@ import { reconcile } from "./reconciler";
 export class Component {
   constructor(props) {
     this.props = props;
-    this.state = this.state || {};
   }
-  setState(partialState) {
-    this.state = Object.assign({}, this.state, partialState);
-    updateInstance(this.__internalInstance);
-  }
-}
-function updateInstance(internalInstance) {
-  const parentDom = internalInstance.dom.parentNode;
-  const element = internalInstance.element;
-  reconcile(parentDom, internalInstance, element);
 }
--- packages/ryunix/lib/reconciler.js
+++ packages/ryunix/lib/reconciler.js
@@ -12,28 +12,36 @@ export function reconcile(parentDom, instance, element) {
     const newInstance = instantiate(element);
     parentDom.appendChild(newInstance.dom);
     return newInstance;
-  } else if (element == null) {
+  }
+
+  if (element == null) {
     parentDom.removeChild(instance.dom);
     return null;
-  } else if (instance.element.type !== element.type) {
+  }
+
+  if (instance.element.type !== element.type) {
     const newInstance = instantiate(element);
-    parentDom.replaceChild(newInstance.dom, instance.dom);
+    parentDom.replaceChild(instance.dom, newInstance.dom);
     return newInstance;
-  } else if (typeof element.type === "string") {
+  }
+
+  if (typeof element.type === "string") {
     instance.childInstances = reconcileChildren(instance, element);
     instance.element = element;
     return instance;
-  } else {
-    instance.publicInstance.props = element.props;
-    const childElement = instance.publicInstance.render();
-    const oldChildInstance = instance.childInstance;
-    const childInstance = reconcile(parentDom, oldChildInstance, childElement);
-    instance.dom = childInstance.dom;
-    instance.childInstance = childInstance;
-    instance.element = element;
-    return instance;
   }
+
+  // Si no es una cadena, asumimos que es un componente personalizado
+  instance.publicInstance.props = element.props;
+  const childElement = instance.publicInstance.render();
+  const oldChildInstance = instance.childInstance;
+  const childInstance = reconcile(parentDom, oldChildInstance, childElement);
+  instance.dom = childInstance.dom;
+  instance.childInstance = childInstance;
+  instance.element = element;
+  return instance;
 }
+
 function instantiate(element) {
   const { type, props } = element;
   const isDomElement = typeof type === "string";
@@ -74,17 +82,21 @@ function createPublicInstance(element, internalInstance) {
   publicInstance.__internalInstance = internalInstance;
   return publicInstance;
 }
+
 function reconcileChildren(instance, element) {
-  const dom = instance.dom;
-  const childInstances = instance.childInstances;
+  const { dom, childInstances } = instance;
   const nextChildElements = element.props.children || [];
+
   const newChildInstances = [];
+
   const count = Math.max(childInstances.length, nextChildElements.length);
+
   for (let i = 0; i < count; i++) {
     const childInstance = childInstances[i];
     const childElement = nextChildElements[i];
     const newChildInstance = reconcile(dom, childInstance, childElement);
     newChildInstances.push(newChildInstance);
   }
-  return newChildInstances.filter((instance) => instance != null);
+
+  return newChildInstances.filter(Boolean);
 }
