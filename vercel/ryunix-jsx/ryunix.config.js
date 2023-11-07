85a91deff7b0589f18203fa028af535bdc9f6483
Neyunse <neyunse@kagarisoft.com>
2023-11-07 19:17:38 -0300

    chore: delete vercel/ryunix-jsx/public/index.html and robots.txt

---
+++ vercel/ryunix-jsx/ryunix.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // used in dev and build mode
+  static: {
+    favicon: true, // if is false the favicon is not mandatory
+    seo: {
+      title: "Ryunix App",
+      meta: {
+        description: "Web site created using @unsetsoft/cra",
+      },
+    },
+  },
+};
