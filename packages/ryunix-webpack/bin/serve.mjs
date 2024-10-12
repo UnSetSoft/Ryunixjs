b383b0fa125d95b18835747c6bf9f65c34e3d227
Max <maximilianotoledo345@gmail.com>
2024-10-11 22:04:06 -0300

    update packages/ryunix-webpack/bin/index.mjs, serve.mjs and config.cjs console.log

--- packages/ryunix-webpack/bin/serve.mjs
+++ packages/ryunix-webpack/bin/serve.mjs
@@ -1,24 +1,41 @@
 import Webpack from 'webpack'
 import WebpackDevServer from 'webpack-dev-server'
 import webpackConfig from '../webpack.config.mjs'
+import configFile from '../utils/settingfile.cjs'
+import envPath from "../utils/envExist.cjs"
+
 import logger from 'terminal-log'
+
 const StartServer = async (cliSettings) => {
   webpackConfig.mode = 'development'
   const compiler = Webpack(webpackConfig)
   const devServerOptions = { ...webpackConfig.devServer, ...cliSettings }
   const server = new WebpackDevServer(devServerOptions, compiler)
 
-  await server.startCallback((err) => {
-    if (err) {
-      return logger.error(`[error] ${err.message}`)
-    }
+  let port = webpackConfig.devServer.port || 3000;
 
-    if (webpackConfig.mode === 'development') {
-      logger.info(
-        `[info] Dev Server is runing at: http://localhost:${webpackConfig.devServer.port}`,
-      )
+  const startServer = async () => {
+    try {
+      await server.start(port);
+      logger.info("<<RyunixJS>>");
+      logger.info(`
+          - Runing at: http://localhost:${port}
+          ${configFile ? `- Config file loaded` : null}
+          ${envPath() ? `- Enviroment: .env`: null}
+      `)
+      logger.info(`🚀 Launched in ${Date.now() - startTime}ms`)
+    } catch (err) {
+      logger.error(`[error] ${err.message}`);
+      if (err.code === 'EADDRINUSE') {
+        port += 1;
+        logger.warn(`Port ${port - 1} is in use, trying ${port} instead.`);
+        await startServer();  // Intenta de nuevo con un puerto diferente
+      }
     }
-  })
+  }
+
+  const startTime = Date.now();
+  await startServer();
 }
 
 export { StartServer }
--- packages/ryunix-webpack/utils/config.cjs
+++ packages/ryunix-webpack/utils/config.cjs
@@ -1,27 +1,5 @@
 'use strict'
-const fs = require('fs')
-const path = require('path')
-const defaultConfigFile = path.join(
-  __dirname,
-  '../../../../',
-  'ryunix.config.js',
-)
-
-const CommonConfigFile = path.join(
-  __dirname,
-  '../../../../',
-  'ryunix.config.cjs',
-)
-
-let config = {}
-
-if (fs.existsSync(defaultConfigFile)) {
-  config = require('../../../../ryunix.config.js')
-  console.log('[info] configuration file was found.')
-} else if (fs.existsSync(CommonConfigFile)) {
-  config = require('../../../../ryunix.config.cjs')
-  console.log('[info] configuration file was found.')
-}
+const { config } = require("./settingfile.cjs")
 
 const defaultSettings = {
   static: {
