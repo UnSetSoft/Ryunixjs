11eabdf62c82e128c4923de4e494b844bce48d2b
Max <maximilianotoledo345@gmail.com>
2024-10-11 22:47:25 -0300

    update 5 files

--- packages/ryunix-webpack/bin/serve.mjs
+++ packages/ryunix-webpack/bin/serve.mjs
@@ -3,8 +3,9 @@ import WebpackDevServer from 'webpack-dev-server'
 import webpackConfig from '../webpack.config.mjs'
 import configFile from '../utils/settingfile.cjs'
 import envPath from '../utils/envExist.cjs'
-
+import { getPackageVersion } from '../utils/index.mjs'
 import logger from 'terminal-log'
+import chalk from 'chalk'
 
 const StartServer = async (cliSettings) => {
   webpackConfig.mode = 'development'
@@ -14,21 +15,28 @@ const StartServer = async (cliSettings) => {
 
   let port = webpackConfig.devServer.port || 3000
 
+  const { version } = await getPackageVersion()
+
   const startServer = async () => {
     try {
       await server.start(port)
-      logger.info('<<RyunixJS>>')
       logger.info(`
+        ${chalk.bold(chalk.cyanBright(`<Ryunix/> ${version}`))}
+
           - Runing at: http://localhost:${port}
           ${configFile ? `- Config file loaded` : null}
-          ${envPath() ? `- Enviroment: .env` : null}
+          ${envPath() ? `- Environments: .env` : null}
+          - Launched in: 🚀 ${Date.now() - startTime}ms
       `)
-      logger.info(`🚀 Launched in ${Date.now() - startTime}ms`)
     } catch (err) {
       logger.error(`[error] ${err.message}`)
       if (err.code === 'EADDRINUSE') {
         port += 1
-        logger.warn(`Port ${port - 1} is in use, trying ${port} instead.`)
+        logger.warn(
+          `${chalk.yellow('⚠️')} Port ${
+            port - 1
+          } is in use, trying ${port} instead.`,
+        )
         await startServer() // Intenta de nuevo con un puerto diferente
       }
     }
