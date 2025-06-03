4e2aaf03a7d659bbc456c204b6b3724363c1db50
Neyunse <neyunse@kagarisoft.com>
2025-06-02 23:26:25 -0300

    update package.json and vite.config.js

--- packages/cra/templates/Vite/vite.config.js
+++ packages/cra/templates/Vite/vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   esbuild: {
     jsxFactory: 'Ryunix.createElement',
-    jsxFragment: 'Fragment',
+    jsxFragment: 'Ryunix.Fragment',
   },
 })
