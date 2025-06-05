c43c0ec4d2e02b82fe7383845f66a2b031ce86c1
Neyunse <maximilianotoledo345@gmail.com>
2025-06-05 04:01:38 -0300

    update: improvements

--- packages/ryunix-webpack/bin/index.mjs
+++ packages/ryunix-webpack/bin/index.mjs
@@ -3,6 +3,9 @@ import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 import { StartServer } from './serve.mjs'
 import { compiler } from './compiler.mjs'
+import logger from 'terminal-log'
+import chalk from 'chalk'
+import defaultSettings from '../utils/config.cjs'
 
 const serv = {
   command: 'server',
@@ -21,18 +24,27 @@ const build = {
   command: 'build',
   describe: 'Run builder',
   handler: async (arg) => {
+    if (!defaultSettings.webpack.production) {
+      logger.error(
+        chalk.red(
+          'The compilation cannot complete because you are trying to compile in developer mode. remember update ryunix.config.js.',
+        ),
+      )
+      return
+    }
+
     compiler.run((err, stats) => {
       if (err || stats.hasErrors()) {
-        // ...
-        console.error(err)
+        logger.error(chalk.red('Error during compilation:'))
+        logger.error(err || stats.toString('errors-only'))
+        return
       }
 
-      console.log('Deployment completed')
+      logger.info(chalk.green('Compilation successful! 🎉'))
 
       compiler.close((closeErr) => {
         if (closeErr) {
-          // ...
-          console.error(closeErr)
+          logger.error(chalk.red('Error closing the compiler:'), closeErr)
         }
       })
     })
