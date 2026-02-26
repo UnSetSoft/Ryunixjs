de5d30066b8d912a1fdc8de848f7d00dab0501ae
Neyunse <neyunse@kagarisoft.com>
2026-02-25 21:02:51 -0300

    feat: Add initial Ryunix Webpack configuration file.

--- packages/cra/templates/Webpack/ryunix.config.js
+++ packages/cra/templates/Webpack/ryunix.config.js
@@ -4,12 +4,6 @@ const RyunixSettings = {
   // used in dev and build mode
   static: {
     favicon: true, // if is false the favicon is not mandatory
-    seo: {
-      title: 'Ryunix App', // include a default title for the site
-      meta: {
-        description: 'Web site created using @unsetsoft/cra',
-      },
-    },
   },
   webpack: {
     production: false, // use 'true' for production
