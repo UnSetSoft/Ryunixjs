0c8542d7aa284ab784bcdf02837d6a6c1d0493a2
Neyunse <neyunse@kagarisoft.com>
2026-03-13 23:46:18 -0300

    removed file

--- test-path.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-const buildDir = '.ryunix';
-const routePath = '/blog/demo';
-const outputDir = path.join(buildDir, 'static', routePath);
-console.log('OutputDir:', outputDir);
-console.log('Resolved:', path.resolve(outputDir));
