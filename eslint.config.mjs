7a49ee0652866c47571cc2b32d7b271f5df87b16
Neyunse <maximilianotoledo345@gmail.com>
2026-05-25 21:08:15 -0300

    chore: add core package configuration and update ESLint ignore patterns

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,7 @@ export default [
       '**/.turbo/**',
       '**/.ryunix/**',
       '**/dist/**',
+      '**/out/**',
       'test/**',
       '_ci/**',
       'packages/ryunix-vscode/syntaxes/**',
